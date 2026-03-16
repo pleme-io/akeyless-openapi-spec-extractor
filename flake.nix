58f6c9952692a1a5d72348d0185bb02627ecfee3
drzzln <drzzln@protonmail.com>
2026-03-15 21:09:26 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -1,30 +1,19 @@
 {
   description = "CLI tool for extracting single endpoints from OpenAPI specs with all endpoint details";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    substrate = { url = "github:pleme-io/substrate"; inputs.nixpkgs.follows = "nixpkgs"; };
     flake-utils.url = "github:numtide/flake-utils";
   };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      packages.default = pkgs.buildNpmPackage {
-        pname = "akeyless-openapi-spec-extractor";
-        version = "0.0.0-dev";
-        src = self;
-        npmDepsHash = "sha256-VSFu7jZapXDzkPP2HKdin1XgvCXepRvxervzrJkTf34=";
-        dontNpmBuild = true;
-        meta = {
-          description = "CLI tool for extracting single endpoints from OpenAPI specs with all endpoint details";
-          homepage = "https://github.com/pleme-io/akeyless-openapi-spec-extractor";
-          license = pkgs.lib.licenses.asl20;
-        };
-      };
-
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ nodejs_22 ];
-      };
-    });
+  outputs = inputs: (import "${inputs.substrate}/lib/repo-flake.nix" {
+    inherit (inputs) nixpkgs flake-utils;
+  }) {
+    self = inputs.self;
+    language = "npm";
+    builder = "package";
+    pname = "akeyless-openapi-spec-extractor";
+    npmDepsHash = "sha256-VSFu7jZapXDzkPP2HKdin1XgvCXepRvxervzrJkTf34=";
+    dontNpmBuild = true;
+    description = "CLI tool for extracting single endpoints from OpenAPI specs with all endpoint details";
+  };
 }
